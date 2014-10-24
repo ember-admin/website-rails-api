46f51ec48575f2cc54a893e69c9c69437ea48b79
opak <opak.alexandr@gmail.com>
2014-10-24 20:53:54 +0300

    Update avatar_uploader.rb

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,4 +12,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg jpg gif png)
   end
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
