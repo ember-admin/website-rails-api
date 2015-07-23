057895a959d39c367e685786242c4fef9cca848d
Opak Alex <opak.alexandr@gmail.com>
2015-07-23 09:09:24 +0300

    fix server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{178.62.247.227}
-role :web, %w{178.62.247.227}
+role :app, %w{91.221.70.59}
+role :web, %w{91.221.70.59}
 
 # Extended Server Syntax
 # ======================
@@ -13,7 +13,7 @@ role :web, %w{178.62.247.227}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '178.62.247.227', user: 'deploy', roles: %w{web app}
+server '91.221.70.59', user: 'deploy', roles: %w{web app}
 
 set :ssh_options, {
     forward_agent: true
